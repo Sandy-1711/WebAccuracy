fefaa8f7d9ddb271ab6ad4448ed2ac93a6003747
Sandy-1711 <sandy1711003@gmail.com>
2023-09-03 14:07:37 +0530

    responsive 100%

--- src/app/components/Nav/nav.js
+++ src/app/components/Nav/nav.js
@@ -8,6 +8,15 @@ import { useEffect } from "react"
 export default function Nav() {
 
     useEffect(function () {
+
+        window.addEventListener('mousemove', function (e) {
+            const mousePosition = e.clientX / window.innerWidth * 100;
+            if (mousePosition > 50) {
+                document.getElementById("menubar").style.display = "block";
+                document.getElementById("menuclose").style.display = "none";
+                document.getElementById("menumobile").style.transform = "translate(-50dvw)"
+            }
+        })
         if (window.innerWidth > 900) {
             gsap.registerPlugin(ScrollTrigger);
             var tl = gsap.timeline();
@@ -31,24 +40,7 @@ export default function Nav() {
         <div className={navcss.navinner}>
 
             <div className={navcss.logo}><Image height={100} width={100} src='https://zimed.netlify.app/assets/images/logo-1-1.png' alt="logo" /></div>
-            <div className={navcss.menuicon}>
 
-                <img className={navcss.menubar} id="menubar" onClick={function () {
-                    document.getElementById("menuclose").style.display = "block";
-                    document.getElementById("menubar").style.display = "none";
-                    document.getElementById("nav").style.transform = "translate(0)"
-                }} src="/menu.svg" alt="menu" />
-            </div>
-            <div className={navcss.menuicon}>
-
-                <img className={navcss.menuclose}
-                    onClick={function () {
-                        document.getElementById("menubar").style.display = "block";
-                        document.getElementById("menuclose").style.display = "none";
-                        document.getElementById("nav").style.transform = "translate(-40dvw)"
-
-                    }} id="menuclose" src="/close.svg" alt="close" />
-            </div>
 
             <ul className={navcss.menu}>
                 <li>
@@ -104,7 +96,84 @@ export default function Nav() {
 
             </ul>
             <button>Start Now</button>
+        </div>
+        <div id="menumobile" className={navcss.menumobile}>
 
+            <div className={navcss.logomobile}><Image height={100} width={100} src='https://zimed.netlify.app/assets/images/logo-1-1.png' alt="logo" /></div>
+            <div className={navcss.menuicon}>
+
+                <img className={navcss.menubar} id="menubar" onClick={function () {
+                    document.getElementById("menuclose").style.display = "block";
+                    document.getElementById("menubar").style.display = "none";
+                    document.getElementById("menumobile").style.transform = "translate(0)"
+                }} src="/menu.svg" alt="menu" />
+            </div>
+            <div className={navcss.menuicon}>
+
+                <img className={navcss.menuclose}
+                    onClick={function () {
+                        document.getElementById("menubar").style.display = "block";
+                        document.getElementById("menuclose").style.display = "none";
+                        document.getElementById("menumobile").style.transform = "translate(-50dvw)"
+
+                    }} id="menuclose" src="/close.svg" alt="close" />
+            </div>
+
+            <ul className={navcss.menulist}>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li className={navcss.servicestab}>
+
+                    <a>Services <i class="fa-solid fa-chevron-right"></i></a>
+
+                    <div className={navcss.submenu}>
+                        <ul>
+                            <li><a href='/services#app-dev'>App Development</a></li>
+                            <li><a href='/services#web-dev'>Web Development</a></li>
+                            <li><a href='/services#ui-ux'>UI/UX Design</a></li>
+                            <li><a href='/services#digital-marketing'>Digital Marketing</a></li>
+                            <li><a href='/services#adshooting-photography'>Ad shooting/Photography</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li className={navcss.trendstab}>
+                    <a>Web 3.0 & trends <i class="fa-solid fa-chevron-right"></i></a>
+                    <div className={navcss.submenu}>
+
+                        <ul>
+                            <li><a href='/trends#iot'>IOT</a></li>
+                            <li><a href='/trends#ai/ml'>AI/ML</a></li>
+                            <li><a href='/trends#blockchain'>Blockchain</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li className={navcss.threedtechtab}>
+                    <a>3D reality <i class="fa-solid fa-chevron-right"></i></a>
+                    <div className={navcss.submenu}>
+                        <ul>
+                            <li><a href='/3dtech#ar'>AR</a></li>
+                            <li><a href='/3dtech#vr'>VR</a></li>
+                            <li><a href='/3dtech#mr'>MR</a></li>
+                            <li><a href='/3dtech#dolbyatmos'>Dolby Atmos</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a>Our Clients</a>
+                </li>
+                <li>
+                    <a>Pricing</a>
+                </li>
+                <li>
+                    <a>Get In Touch</a>
+                </li>
+
+            </ul>
         </div>
     </nav>
 }
